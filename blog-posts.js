fb7092894aac459c35240094d598627b719a5c03
Trey Liebscher <wcliebsc@gmail.com>
2018-06-12 07:37:14 -0600

    added PUT and DELETE methods for blogPostRouter

--- blog-posts.js
+++ blog-posts.js
@@ -27,10 +27,38 @@ router.post('/', jsonParser, (req, res) => {
     res.status(201).json(item);
 });
 
+router.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post \'${req.params.id}\'`)
+    res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'author', 'content', 'id'];
+    for (let i = 0; i<requiredFields.length; i++){
+        const field = requiredFields[i];
+        if (!(field in req.body)){
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = `Request path id \`${req.params.id}\` and request body id \`${req.body.id}\` must match`
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    console.log(`Updating blog post \`${req.params.id}\``);
+    const updatedPost = BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content
+    });
+    res.status(204).end();
+});
+
 module.exports=router;
 
-// reate: function(title, content, author, publishDate)
 
-// const item = Recipes.create(req.body.name, req.body.ingredients);
-// res.status(201).json(item);
 
--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const morgan = require('morgan');
 
 const app = express();
 
-const getAndPostBlogRouter = require('./blog-posts');
-const deleteAndUpdateBlogRouter = require('./blog-posts');
+const blogPostRouter = require('./blog-posts');
 
-app.use('/blog-posts', getAndPostBlogRouter);
+app.use('/blog-posts', blogPostRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
