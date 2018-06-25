b1d4ecac2a8084bd8ce445602f4ccdb7647862fa
Trey Liebscher <wcliebsc@gmail.com>
2018-06-25 13:36:50 -0400

    finished tests and added travis file

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, closeServer} = require('../server');
+const {app, runServer, closeServer} = require('./server');
 
 const expect = chai.expect;
 
@@ -19,7 +19,7 @@ describe('Blog Posts', function() {
     it('should list blog posts on GET', function() {
         return chai
             .request(app)
-            .get('/blog-post')
+            .get('/blog-posts')
             .then(function(res) {
                 expect(res).to.have.status(200);
                 expect(res).to.be.json;
@@ -31,5 +31,63 @@ describe('Blog Posts', function() {
                     expect(item).to.include.keys(expectedKeys);
                 });
             });
+    });
+
+    it('should add blog posts on POST', function() {
+        const newPost = {title: 'A new post', author: 'Bill', content: 'This is for testing'};
+        return chai
+            .request(app)
+            .post('/blog-posts')
+            .send(newPost)
+            .then(function(res) {
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('object');
+                expect(res.body).include.keys('title', 'content', 'author');
+                expect(res.body.id).to.not.equal(null);
+                expect(res.body.publishDate).to.not.equal(null);
+                expect(res.body).to.deep.equal(
+                    Object.assign(newPost, {id: res.body.id, publishDate: res.body.publishDate})
+                );
+            });
+    });
+
+    it('should update blog posts on PUT', function() {
+        const updatePost = {
+            title: 'testing post',
+            author: 'tester',
+            content: 'this is for testing purposes'
+        };
+
+        return (
+            chai
+                .request(app)
+                .get('/blog-posts')
+                .then(function(res) {
+                    updatePost.id = res.body[0].id;
+                    return chai
+                        .request(app)
+                        .put(`/blog-posts/${updatePost.id}`)
+                        .send(updatePost);
+                })
+
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                })
+        );
+    });
+
+    it('should delete blog posts on DELETE', function() {
+        return (
+            chai
+                .request(app)
+                .get('/blog-posts')
+                .then(function(res) {
+                    return chai.request(app).delete(`/blog-posts/${res.body[0].id}`);
+                })
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                })
+        );
     });
 });
